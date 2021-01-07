fb4f55d958c819bca4331022ef1e34f0640a4097
Nadia Salmen <nadiasalmen@gmail.com>
2021-01-07 10:37:05 -0300

    User story: As a user, I can edit a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # read all
   get 'tasks', to: 'tasks#index', as: :tasks
 
+  # create one
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
 
+  # read one - show must go after the "new" routes
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # edit one
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 end
